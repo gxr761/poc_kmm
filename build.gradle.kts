8b6557f680dfd7c26a63a0bf04e2a4249aa16054
Guilherme <guilhermerocs@gmail.com>
2024-08-13 14:36:43 -0300

    ios 15 attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
         version = "1.0"
-        ios.deploymentTarget = "16.0"
+        ios.deploymentTarget = "15.0"
         framework {
             baseName = "shared"
             isStatic = true
